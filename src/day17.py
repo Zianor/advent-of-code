37dfdc8627e22e76807b0bcd1831101de6fe529c
Cay Rahn <cay.rahn@rwth-aachen.de>
2021-01-03 19:42:45 +0100

    day 17

---
+++ src/day17.py
@@ -0,0 +1,539 @@
+import numpy as np
+
+from helper import get_data
+
+data = [list(line) for line in open(get_data(17), 'r').read().split('\n')]
+data = np.char.asarray(data, itemsize=1)
+"""
+--- Day 17: Conway Cubes ---
+
+As your flight slowly drifts through the sky, the Elves at the Mythical Information Bureau at the North Pole contact 
+you. They'd like some help debugging a malfunctioning experimental energy source aboard one of their super-secret 
+imaging satellites.
+
+The experimental energy source is based on cutting-edge technology: a set of Conway Cubes contained in a pocket 
+dimension! When you hear it's having problems, you can't help but agree to take a look.
+
+The pocket dimension contains an infinite 3-dimensional grid. At every integer 3-dimensional coordinate (x,y,z), there 
+exists a single cube which is either active or inactive.
+
+In the initial state of the pocket dimension, almost all cubes start inactive. The only exception to this is a small 
+flat region of cubes (your puzzle input); the cubes in this region start in the specified active (#) or inactive (.) 
+state.
+
+The energy source then proceeds to boot up by executing six cycles.
+
+Each cube only ever considers its neighbors: any of the 26 other cubes where any of their coordinates differ by at most 
+1. For example, given the cube at x=1,y=2,z=3, its neighbors include the cube at x=2,y=2,z=2, the cube at x=0,y=2,z=3, 
+and so on.
+
+During a cycle, all cubes simultaneously change their state according to the following rules:
+
+If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active. Otherwise, the cube 
+becomes inactive.
+If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active. Otherwise, the cube remains 
+inactive.
+The engineers responsible for this experimental energy source would like you to simulate the pocket dimension and 
+determine what the configuration of cubes should be at the end of the six-cycle boot process.
+
+For example, consider the following initial state:
+
+.#.
+..#
+###
+
+Even though the pocket dimension is 3-dimensional, this initial state represents a small 2-dimensional slice of it. (In 
+particular, this initial state defines a 3x3x1 region of the 3-dimensional space.)
+
+Simulating a few cycles from this initial state produces the following configurations, where the result of each cycle 
+is shown layer-by-layer at each given z coordinate (and the frame of view follows the active cells in each cycle):
+
+Before any cycles:
+
+z=0
+.#.
+..#
+###
+
+
+After 1 cycle:
+
+z=-1
+#..
+..#
+.#.
+
+z=0
+#.#
+.##
+.#.
+
+z=1
+#..
+..#
+.#.
+
+
+After 2 cycles:
+
+z=-2
+.....
+.....
+..#..
+.....
+.....
+
+z=-1
+..#..
+.#..#
+....#
+.#...
+.....
+
+z=0
+##...
+##...
+#....
+....#
+.###.
+
+z=1
+..#..
+.#..#
+....#
+.#...
+.....
+
+z=2
+.....
+.....
+..#..
+.....
+.....
+
+
+After 3 cycles:
+
+z=-2
+.......
+.......
+..##...
+..###..
+.......
+.......
+.......
+
+z=-1
+..#....
+...#...
+#......
+.....##
+.#...#.
+..#.#..
+...#...
+
+z=0
+...#...
+.......
+#......
+.......
+.....##
+.##.#..
+...#...
+
+z=1
+..#....
+...#...
+#......
+.....##
+.#...#.
+..#.#..
+...#...
+
+z=2
+.......
+.......
+..##...
+..###..
+.......
+.......
+.......
+After the full six-cycle boot process completes, 112 cubes are left in the active state.
+
+Starting with your given initial configuration, simulate six cycles. How many cubes are left in the active state after 
+the sixth cycle?
+"""
+
+data = [data]
+
+
+def get_active(data):
+    active = set()
+    for x, layer in enumerate(data):
+        for y, row in enumerate(layer):
+            for z, cube in enumerate(row):
+                if cube == '#':
+                    active.add((x, y, z))
+    return active, (0, len(data)), (0, len(layer)), (0, len(row))
+
+
+def count_active_neighbours(pos, active):
+    x, y, z = pos
+    count = 0
+    for x_neighbour in range(x - 1, x + 2):
+        for y_neighbour in range(y - 1, y + 2):
+            for z_neighbour in range(z - 1, z + 2):
+                if (x_neighbour, y_neighbour, z_neighbour) in active and (x_neighbour, y_neighbour, z_neighbour) != pos:
+                    count += 1
+    return count
+
+
+def calc_state(active, x_range, y_range, z_range):
+    x_range_new = (x_range[0] - 1, x_range[1] + 1)
+    y_range_new = (y_range[0] - 1, y_range[1] + 1)
+    z_range_new = (z_range[0] - 1, z_range[1] + 1)
+    active_new = set()
+    for x in range(x_range[0] - 1, x_range[1] + 2):
+        for y in range(y_range[0] - 1, y_range[1] + 2):
+            for z in range(z_range[0] - 1, z_range[1] + 2):
+                pos = (x, y, z)
+                count = count_active_neighbours(pos, active)
+                if pos in active and 2 <= count <= 3:
+                    active_new.add(pos)
+                elif pos not in active and count == 3:
+                    active_new.add(pos)
+    return active_new, x_range_new, y_range_new, z_range_new
+
+
+def draw_image(active, x_range, y_range, z_range):
+    for x in range(x_range[0], x_range[1]):
+        print(f"z={x}")
+        for y in range(y_range[0], y_range[1]):
+            line = ""
+            for z in range(z_range[0], z_range[1]):
+                if (x, y, z) in active:
+                    line += '#'
+                else:
+                    line += '.'
+            print(line)
+
+
+active, x_range, y_range, z_range = get_active(data)
+for i in range(6):
+    active, x_range, y_range, z_range = calc_state(active, x_range, y_range, z_range)
+
+print(f"{len(active)} cubes are left in active state")
+
+
+"""
+--- Part Two ---
+
+For some reason, your simulated results don't match what the experimental energy source engineers expected. Apparently, 
+the pocket dimension actually has four spatial dimensions, not three.
+
+The pocket dimension contains an infinite 4-dimensional grid. At every integer 4-dimensional coordinate (x,y,z,w), 
+there exists a single cube (really, a hypercube) which is still either active or inactive.
+
+Each cube only ever considers its neighbors: any of the 80 other cubes where any of their coordinates differ by at most 
+1. For example, given the cube at x=1,y=2,z=3,w=4, its neighbors include the cube at x=2,y=2,z=3,w=3, the cube at 
+x=0,y=2,z=3,w=4, and so on.
+
+The initial state of the pocket dimension still consists of a small flat region of cubes. Furthermore, the same rules 
+for cycle updating still apply: during each cycle, consider the number of active neighbors of each cube.
+
+For example, consider the same initial state as in the example above. Even though the pocket dimension is 4-dimensional,
+this initial state represents a small 2-dimensional slice of it. (In particular, this initial state defines a 3x3x1x1 
+region of the 4-dimensional space.)
+
+Simulating a few cycles from this initial state produces the following configurations, where the result of each cycle is
+shown layer-by-layer at each given z and w coordinate:
+
+Before any cycles:
+
+z=0, w=0
+.#.
+..#
+###
+
+
+After 1 cycle:
+
+z=-1, w=-1
+#..
+..#
+.#.
+
+z=0, w=-1
+#..
+..#
+.#.
+
+z=1, w=-1
+#..
+..#
+.#.
+
+z=-1, w=0
+#..
+..#
+.#.
+
+z=0, w=0
+#.#
+.##
+.#.
+
+z=1, w=0
+#..
+..#
+.#.
+
+z=-1, w=1
+#..
+..#
+.#.
+
+z=0, w=1
+#..
+..#
+.#.
+
+z=1, w=1
+#..
+..#
+.#.
+
+
+After 2 cycles:
+
+z=-2, w=-2
+.....
+.....
+..#..
+.....
+.....
+
+z=-1, w=-2
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=-2
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=1, w=-2
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=-2
+.....
+.....
+..#..
+.....
+.....
+
+z=-2, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=-1, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=1, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=-2, w=0
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=-1, w=0
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=0
+.....
+.....
+.....
+.....
+.....
+
+z=1, w=0
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=0
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=-2, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=-1, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=1, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=-2, w=2
+.....
+.....
+..#..
+.....
+.....
+
+z=-1, w=2
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=2
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=1, w=2
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=2
+.....
+.....
+..#..
+.....
+.....
+After the full six-cycle boot process completes, 848 cubes are left in the active state.
+
+Starting with your given initial configuration, simulate six cycles in a 4-dimensional space. How many cubes are left 
+in the active state after the sixth cycle?
+"""
+
+data = [data]  # already 3d
+
+
+def get_active_4d(data):
+    active = set()
+    for w, space_3d in enumerate(data):
+        for x, layer in enumerate(space_3d):
+            for y, row in enumerate(layer):
+                for z, cube in enumerate(row):
+                    if cube == '#':
+                        active.add((w, x, y, z))
+    return active, (0, len(data)), (0, len(space_3d)), (0, len(layer)), (0, len(row))
+
+
+def count_active_neighbours_4d(pos, active):
+    w, x, y, z = pos
+    count = 0
+    for w_neighbour in range(w - 1, w + 2):
+        for x_neighbour in range(x - 1, x + 2):
+            for y_neighbour in range(y - 1, y + 2):
+                for z_neighbour in range(z - 1, z + 2):
+                    curr_pos = (w_neighbour, x_neighbour, y_neighbour, z_neighbour)
+                    if curr_pos in active and curr_pos != pos:
+                        count += 1
+    return count
+
+
+def calc_state_4d(active, w_range, x_range, y_range, z_range):
+    w_range_new = (w_range[0] - 1, w_range[1] + 1)
+    x_range_new = (x_range[0] - 1, x_range[1] + 1)
+    y_range_new = (y_range[0] - 1, y_range[1] + 1)
+    z_range_new = (z_range[0] - 1, z_range[1] + 1)
+    active_new = set()
+    for w in range(w_range[0] - 1, w_range[1] + 2):
+        for x in range(x_range[0] - 1, x_range[1] + 2):
+            for y in range(y_range[0] - 1, y_range[1] + 2):
+                for z in range(z_range[0] - 1, z_range[1] + 2):
+                    pos = (w, x, y, z)
+                    count = count_active_neighbours_4d(pos, active)
+                    if pos in active and 2 <= count <= 3:
+                        active_new.add(pos)
+                    elif pos not in active and count == 3:
+                        active_new.add(pos)
+    return active_new, w_range_new, x_range_new, y_range_new, z_range_new
+
+
+active, w_range, x_range, y_range, z_range = get_active_4d(data)
+for i in range(6):
+    active, w_range, x_range, y_range, z_range = calc_state_4d(active, w_range, x_range, y_range, z_range)
+
+print(f"{len(active)} cubes are left in active state in 4 dimensions")
